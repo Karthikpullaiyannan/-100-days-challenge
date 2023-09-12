90e68872dfe67f37fb32b2e73734facb8c3407ab
karthik <karthik30112003@gmail.com>
2023-09-12 23:06:38 +0530

    String Slicing in Python

---
+++ 30_string_slicing.py
@@ -0,0 +1,8 @@
+string= "Welcome to my world"
+#str[start:end:step]
+print(string[0:])
+print(string[:5])
+print(string[2:8])
+print(string[::2])
+print(string[-5:-1])
+print(string[::-1]) #reverse a string
