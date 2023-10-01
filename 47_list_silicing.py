0feb2dd76a8066b36e3a137d8cbb2ab7c7ca5f79
karthik <karthik30112003@gmail.com>
2023-10-01 23:54:12 +0530

    Silicing operation in List

---
+++ 47_list_silicing.py
@@ -0,0 +1,7 @@
+#list_1=[int(list_1) for list_1 in input("Enter the values in the string:").split(",")]
+list_1=[23,21,67,43,20,89,100]
+print(list_1)
+print("Reverse a list:",list_1[::-1])
+print(list_1[1::2])
+print(list_1[0:10:3])
+print(list_1[:-1])
